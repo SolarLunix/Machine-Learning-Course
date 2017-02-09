548d0fb5aa7eefb6c252a94828c6f5d2c33c0304
SolarLunix <solarlunix@gmail.com>
2017-02-09 16:58:42 +0000

    Get actual classification for specific tests.

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -21,15 +21,14 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-print "-----------------"
+print "\n-----------------\n"
 
 #########################################################
 ### your code goes here ###
 
 # Reduce training size as per instructions
-# features_train = features_train[:len(features_train)/100]
-# labels_train = labels_train[:len(labels_train)/100]
+features_train = features_train[:len(features_train)/100]
+labels_train = labels_train[:len(labels_train)/100]
 
 # Initialise SVM
 trainer = SVC(C=10000, kernel="rbf")
@@ -44,13 +43,26 @@ t0 = time()
 pred = trainer.predict(features_test)
 print "Prediction time:", round(time()-t0, 3), "s"
 
-print "-----------------"
+print "\n-----------------\n"
 
 # Determine how accurate the prediction is
 score = accuracy_score(labels_test, pred)
 
 # Display prediction score
 print "Accuracy:", score
+
+print "\n-----------------\n"
+
+# Check specific results.
+
+test10 = (pred[10] == labels_test[10])
+test26 = (pred[26] == labels_test[26])
+test50 = (pred[50] == labels_test[50])
+
+print "Test 10 is:", test10, pred[10]
+print "Test 26 is:", test26, pred[26]
+print "Test 50 is:", test50, pred[50]
+
 #########################################################
 
 
