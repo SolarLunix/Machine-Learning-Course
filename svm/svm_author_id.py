ce0cb18f628aeb0490dee01d8f65095db75a2115
SolarLunix <solarlunix@gmail.com>
2017-02-09 16:36:40 +0000

    rbf classifier

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -32,7 +32,7 @@ features_train = features_train[:len(features_train)/100]
 labels_train = labels_train[:len(labels_train)/100]
 
 # Initialise SVM
-trainer = SVC(kernel="linear")
+trainer = SVC(kernel="rbf")
 
 # Train the trainer
 t0 = time()
