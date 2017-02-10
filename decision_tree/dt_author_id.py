ee0a67bb5a5b7251a0600132a3d9f60f6c0bd5fd
SolarLunix <solarlunix@gmail.com>
2017-02-10 12:11:19 +0000

    Finished Decision Tree problem

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -10,6 +10,8 @@
     
 import sys
 from time import time
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -19,13 +21,35 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+print "-----------------"
 
 
 #########################################################
 ### your code goes here ###
 
+# Initialise Trainer
+trainer = DecisionTreeClassifier(min_samples_split=40)
 
+# Train the trainer
+t0 = time()
+trainer.fit(features_train, labels_train)
+print "Training time:", round(time()-t0, 3), "s"
+
+# Predict the result of new data
+t0 = time()
+pred = trainer.predict(features_test)
+print "Prediction time:", round(time()-t0, 3), "s"
+
+print "-----------------"
+
+# Determine how accurate the prediction is
+score = accuracy_score(labels_test, pred)
+
+# Display prediction score
+print "Accuracy:", score
+
+features = len(features_train[0])
+print "Features:", features
 #########################################################
 
 
