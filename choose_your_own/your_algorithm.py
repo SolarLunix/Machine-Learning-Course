84747fe0486831a0207796b58ab8b444256794d2
SolarLunix <solarlunix@gmail.com>
2017-02-12 21:13:08 +0000

    Completed kNN code

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,6 +3,9 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from time import time
+from sklearn.metrics import accuracy_score
+from sklearn.neighbors import KNeighborsClassifier
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -15,7 +18,6 @@ bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 
-
 #### initial visualization
 plt.xlim(0.0, 1.0)
 plt.ylim(0.0, 1.0)
@@ -24,21 +26,39 @@ plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
 plt.legend()
 plt.xlabel("bumpiness")
 plt.ylabel("grade")
-plt.show()
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
-### visualization code (prettyPicture) to show you the decision boundary
+### visualization code (prettyPicture) to show you the decision boundary]
+
+def picture(classifier, imgName):
+    try:
+        prettyPicture(classifier, features_test, labels_test, imgName)
+    except NameError:
+        pass
+    
+
+def kNN():
+    # Train k Nearest Neighbour
+    print "Performing kNN"
+    t0 = time()
+    clf_knn = KNeighborsClassifier(n_neighbors=4)
+    clf_knn.fit(features_train, labels_train)
+    print "Training time:", round(time() - t0, 3), "s"
+
+    # Test k Nearest Neighbour
+    print "Testing kNN"
+    t0 = time()
+    knn_prediction = clf_knn.predict(features_test)
+    print "Prediction time:", round(time() - t0, 3), "s"
+
+    # View Accuracy
+    knn_acc = accuracy_score(labels_test, knn_prediction)
+    print "Accuracy:", knn_acc
+
+    picture(clf_knn, "knnTest.png")
 
 
-
-
-
-
-
-
-try:
-    prettyPicture(clf, features_test, labels_test)
-except NameError:
-    pass
+# Run Machine Learning Methods
+kNN()
