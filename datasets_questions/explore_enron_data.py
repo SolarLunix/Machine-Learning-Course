1bce0b5197231b20568eb6382ac38a91a467a39d
SolarLunix <solarlunix@gmail.com>
2017-02-13 12:41:27 +0000

    Refactor for easier reading

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,22 +19,37 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
-print "People in list:", len(enron_data)
-print "Features per person", len(enron_data.get("METTS MARK"))
+# Print out someone's data so it is easily seen what features can be worked with
+for feature in enron_data.get("METTS MARK"):
+    print feature
 
-num_poi = 0
-num_salaries = 0
-num_defined_emails = 0
+print "\n-----------------------------\n"
+
+# Variables to increment
+num_poi = 0  # Number of persons of interest available to us
+num_salaries = 0  # Number of people's salaries available to us
+num_defined_emails = 0  # Number of people's emails we have
+num_total_pay = 0
+
+# Iterate through the whole list
 for person in enron_data:
+    # Check Persons of interest
     if enron_data.get(person).get("poi") == 1:
         num_poi += 1
 
+    # Check Salaries
     if enron_data.get(person).get("salary") != "NaN":
         num_salaries += 1
 
+    # Check Email Addresses
     if enron_data.get(person).get("email_address") != "NaN":
         num_defined_emails += 1
 
+    # Check Total Payments
+    if enron_data.get(person).get("total_payments") != "NaN":
+        num_total_pay += 1
+
+    # Check data for individual people by looking at their last name
     name = person.split(" ")
     if name[0] == "PRENTICE":
         print "Prentice stock:", enron_data.get(person).get("total_stock_value")
@@ -48,6 +63,11 @@ for person in enron_data:
     elif name[0] == "FASTOW":
         print "Fastow Total Payments:", enron_data.get(person).get("total_payments")
 
+print "\n-----------------------------\n"
+print "Data Tallies:\n"
+print "People in list:", len(enron_data)
+print "Features per person:", len(enron_data.get("METTS MARK"))
 print "Persons of Interest:", num_poi
 print "Number of defined emails:", num_defined_emails
 print "Number of defined salaries:", num_salaries
+print "Number of undefined total payments:", (len(enron_data) - num_total_pay)
