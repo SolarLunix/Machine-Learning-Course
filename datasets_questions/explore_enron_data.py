7872b6a7f06dac962a5501587ce4f756820992b9
SolarLunix <solarlunix@gmail.com>
2017-02-13 13:03:07 +0000

    Gathered information on Enron Employees

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,6 +19,8 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print "\n-----------------------------\n"
+
 # Print out someone's data so it is easily seen what features can be worked with
 for feature in enron_data.get("METTS MARK"):
     print feature
@@ -29,13 +31,16 @@ print "\n-----------------------------\n"
 num_poi = 0  # Number of persons of interest available to us
 num_salaries = 0  # Number of people's salaries available to us
 num_defined_emails = 0  # Number of people's emails we have
-num_total_pay = 0
+num_total_pay = 0  # Number of people with total payments defined
+num_total_pay_poi = 0  # Number of pois with total payments defined
 
 # Iterate through the whole list
 for person in enron_data:
     # Check Persons of interest
     if enron_data.get(person).get("poi") == 1:
         num_poi += 1
+        if enron_data.get(person).get("total_payments") != "NaN":
+            num_total_pay_poi += 1
 
     # Check Salaries
     if enron_data.get(person).get("salary") != "NaN":
@@ -71,3 +76,7 @@ print "Persons of Interest:", num_poi
 print "Number of defined emails:", num_defined_emails
 print "Number of defined salaries:", num_salaries
 print "Number of undefined total payments:", (len(enron_data) - num_total_pay)
+print "Number of poi without total payments undefined:", (num_poi - num_total_pay_poi)
+
+
+print "\n-----------------------------\n"
