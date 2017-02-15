38d12c399afafa7fdb2bae9b28abcf14fe894726
SolarLunix <solarlunix@gmail.com>
2017-02-15 18:53:13 +0000

    Mini Project update

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -15,6 +15,7 @@
 
 import sys
 import pickle
+from sklearn.linear_model import LinearRegression
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 dictionary = pickle.load( open("../final_project/final_project_dataset_modified.pkl", "r") )
@@ -29,7 +30,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -38,10 +39,14 @@ test_color = "b"
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
 
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
 
 
-
-
+print reg.coef_
+print reg.intercept_
+print reg.score(feature_train, target_train)
+print reg.score(feature_test, target_test)
 
 
 
@@ -67,4 +72,4 @@ except NameError:
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
-plt.show()
+#plt.show()
