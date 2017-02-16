5931c198fad7aed5001fc3724dfd7f486516c782
SolarLunix <solarlunix@gmail.com>
2017-02-16 18:12:02 +0000

    Get data after outliers have been removed

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -22,7 +22,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 
 ### list the features you want to look at--first item in the 
 ### list will be the "target" feature
-features_list = ["bonus", "long_term_incentive"]
+features_list = ["bonus", "salary"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -69,6 +69,15 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+print "\tNo outliers:"
+print "Slope:", reg.coef_[0]
+print "Intercept:", reg.intercept_
+print "r-train:", reg.score(feature_train, target_train)
+print "r-test:", reg.score(feature_test, target_test)
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
